8dee04b4ad3cc9ac2f21d24cad9b670e4ba4692c
sam <s.gelernter@gmail.com>
2021-12-28 10:00:40 -0500

    Complete initial skeleton setup

---
+++ frontend/benchbnb.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+document.addEventListener("DOMContentLoaded", () => {
+    const rootEl = document.getElementById('root');
+    const store = {};
+    const Root = <div>Hello! Don't do the bridge thing!</div>
+    ReactDOM.render(Root, rootEl);
+})
